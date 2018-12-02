2077751428e24ad26085008eda5b66a170344fc0
denismukhin <denis.mukhin@raiffeisen.ru>
2018-12-02 23:32:03 +0300

    Dictionary

---
+++ Dictionary/Dictionary/FIOList.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    class FIOList
+    {
+        string fio;
+        List<int> numbers = new List<int>();
+
+        public FIOList(string fio, List<int> numbers)
+        {
+            this.fio = fio;
+            this.numbers = numbers;
+        }
+
+        public string getFIO()
+        {
+            return fio;
+        }
+        public void print()
+        {
+            Console.WriteLine(fio);
+            foreach (int number in numbers)
+            {
+                Console.WriteLine(number + " ");
+            }
+
+
+        }
+
+        public List<int> returnList()
+        {
+            return numbers;
+        }
+    }
+}
